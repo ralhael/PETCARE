8666bad256dcaa8ac0308bba434cab5fc9c53790
Cauã Ferreira <166433966+cauaferreiraz@users.noreply.github.com>
2026-05-06 23:22:53 -0300

    Separando telas de cliente/admin

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,13 +1,20 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true, // NAO SEI QUE MERDA É ESSA MAS DEIXA
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('frontend');
+
+  constructor(public router: Router) {}
+
+  showHeader(): boolean {
+    return !this.router.url.includes('/login');
+  }
 }
